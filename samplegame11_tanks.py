a24cfc01e8a35245e9d317a75ed37f2d9733b40c
Connor Sadler <connor.sadler@gmail.com>
2020-05-02 10:43:20 +0100

    make turret longer with mouse wheel

--- samplegame11_tanks.py
+++ samplegame11_tanks.py
@@ -29,6 +29,7 @@ class Tank(SpriteWithImage):
         self.setBounceOfEdgeOfScreen()
 
         self.setAngle(90)
+        self.firing = False
 
     def move(self):
         #self.moveBy(self.velocity[0], self.velocity[1])
@@ -36,6 +37,9 @@ class Tank(SpriteWithImage):
         self.turret.move()
         # always keep turrent pointing to last known mouse position
         self.turret.pointTo(self.pointTurrentTo)
+
+        if self.firing:
+            self.fireBullet()
 
     def moveDone(self):
         super().moveDone()
@@ -48,29 +52,72 @@ class Tank(SpriteWithImage):
     def pointTurretTo(self, point):
         self.pointTurrentTo = point
 
-    def onClick(self, point):
-        # work out where end of turret is
-        turretVector = self.turret.getTurretVector()
-        endOfTurret = addVectors((self.x, self.y), turretVector)
+    def fireBullet(self):
+        self.turret.fire()
 
-        # Spawn a bullet facing in the same direction as our turret
-        bullet = Bullet(endOfTurret[0], endOfTurret[1], self.turret.getAngle())
-        addSprite(bullet)
+    def setFiring(self, firing):
+        self.firing = firing
+
+    def changeTurret(self, makeLonger):
+        self.turret.changeTurret(makeLonger)
 
 class Turret(SpriteWithImage):
     def __init__(self, tank):
-        self.turretLength = 150
+        self.turretLength = 100
         super().__init__(0, 0, TurrentImageDrawer(self.turretLength))
         self.tank = tank
 
+        self.gunType = GunTypeA()
+
     def move(self):
         self.setLocation(self.tank.getLocation())
+
+    def setTurretLength(self, turretLength):
+        self.turretLength = turretLength
+        imageFilenameOrFilenamesOrImageHandler = TurrentImageDrawer(self.turretLength)
+        self.imageDrawingHelper = SpriteImageDrawingHelper(self, imageFilenameOrFilenamesOrImageHandler)
+
+    def changeTurret(self, makeLonger):
+        self.setTurretLength(self.turretLength + (10 if makeLonger else -10))
 
     def draw(self):
         super().draw()
 
     def getTurretVector(self):
         return angleToVector(self.angle, self.turretLength)
+
+    def fire(self):
+        # work out where end of turret is
+        turretVector = self.getTurretVector()
+        endOfTurret = addVectors((self.tank.x, self.tank.y), turretVector)
+        # Spawn bullets facing in the same direction as our turret
+        self.gunType.fire(self, endOfTurret)
+
+class GunType:
+    def __init__(self):
+        pass
+
+# Wipes from side to side
+class GunTypeA(GunType):
+    def __init__(self):
+        super().__init__()
+        self.sprayOffset = 0
+        self.sprayOffsetAdder = 1
+
+    def fire(self, turret, endOfTurret):
+        self.sprayOffset += self.sprayOffsetAdder
+        if self.sprayOffset > 50 or self.sprayOffset < -50:
+            self.sprayOffsetAdder = self.sprayOffsetAdder * -1
+
+        numBullets = 6
+        halfBullets = numBullets / 2
+        for i in range(numBullets):
+            randomnum = random.randint(1, 20)
+            bullet = Bullet(endOfTurret[0], endOfTurret[1], turret.getAngle() + (5 * (i - halfBullets)) + randomnum)
+            #bullet = Bullet(endOfTurret[0], endOfTurret[1], turret.getAngle() + (5 * (i - halfBullets)) + self.sprayOffset)
+            addSprite(bullet)
+
+
 
 #
 # Draws the look of the turret onto it's internal image
@@ -123,9 +170,14 @@ class MyGameLoop(GameLoop):
         pass
 
     def onEvent(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            clickPosition = pygame.mouse.get_pos()
-            self.tank.onClick(clickPosition)
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            self.tank.setFiring(True)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            self.tank.setFiring(False)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 4:
+            self.tank.changeTurret(True)
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 5:
+            self.tank.changeTurret(False)
         elif event.type == pygame.MOUSEMOTION:
             mousePosition = pygame.mouse.get_pos()
             self.tank.pointTurretTo(mousePosition)
